8610ca0562fb4c3c85a0e4f95709f6e94f552464
javimetal <etchegarayjavier@gmail.com>
2023-05-24 20:59:19 +0800

    try to deploy swagger into vercel

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,23 @@ async function bootstrap() {
     .setDescription('The geonames API description')
     .setVersion('1.0')
     .build();
+  //const document = SwaggerModule.createDocument(app, config);
+  //SwaggerModule.setup('api', app, document);
+
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('swagger', app, document, {
+    customSiteTitle: 'Backend Generator',
+    customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
+  });
 
   app.enableCors();
   await app.listen(configService.get<string>('API_PORT') || 4000);
